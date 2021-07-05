1b8a9ea77dcadda83bad0003fa90bc19ab178c12
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2021-07-05 12:05:33 +0530

    StaticFiles

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
